8abb98f7a35c2b72f20a7d215e39c1de04137798
Martin Redolatti <martin@mrdesktop>
2016-03-05 20:02:28 -0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <boost/format.hpp>
+
+int main()
+{
+    std::cout << boost::format("hola %1%") % 123 << std::endl;
+    return 0;
+}
---
+++ test/test1.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(TestSuite1, Test1)
+{
+    ASSERT_EQ(1, 1);
+}
